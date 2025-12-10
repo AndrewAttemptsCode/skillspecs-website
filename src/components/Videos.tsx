79339535fdd395d4c85517cf1ae93ec267e954a0
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-12-10 18:03:30 +0000

    feat: add state to track current video and list of all videos, side effect to fetch videos from JSON file and display the current video in a youtube embed player and list videos as images

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,8 +1,71 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+type Video = {
+  videoId: string;
+  title: string;
+  description: string;
+  published: string;
+  thumbnail: string;
+};
+
+const PlayerContainer = styled.section`
+  aspect-ratio: 16 / 9;
+`;
+
 const Videos = () => {
+  const [currentVideo, setCurrentVideo] = useState<Video | null>(null);
+  const [videoList, setVideoList] = useState<Video[]>([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const url =
+          "https://raw.githubusercontent.com/AndrewAttemptsCode/youtube-video-fetcher/main/data.json";
+        const response = await fetch(url);
+        const videoData = (await response.json()) as Video[];
+
+        if (!videoData) {
+          console.error("Failed to fetch video list");
+          return;
+        }
+
+        setCurrentVideo(videoData[0]);
+        setVideoList(videoData);
+      } catch (err) {
+        if (err instanceof Error) {
+          console.error(err.message);
+        } else {
+          console.error(err);
+        }
+      }
+    };
+
+    fetchVideos();
+  }, []);
+
+  if (!currentVideo) return <p>Loading...</p>;
+
   return (
-    <div>
-      This is a video component
-    </div>
+    <>
+      <PlayerContainer>
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://www.youtube.com/embed/${currentVideo.videoId}`}
+          title={currentVideo.title}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </PlayerContainer>
+
+      <section>
+        {videoList.map((video) => (
+          <img key={video.videoId} src={video.thumbnail} alt={video.title} />
+        ))}
+      </section>
+    </>
   );
 };
 
