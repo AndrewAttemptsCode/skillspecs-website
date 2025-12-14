55d2ea02c1b9187a0fbd02ea486c12ba0f889c28
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-12-14 18:51:00 +0000

    feat: add initial decorative background image layer

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,31 +1,31 @@
 import styled from "styled-components";
 import Videos from "./Videos";
 
-const AppWrapper = styled.main`
+const App = styled.main`
   height: 100vh;
   height: 100dvh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  position: relative;
 `;
 
-const Applet = styled.div`
-  border: 2px solid red;
-  border-radius: 8px;
-  width: min(90%, 1448px);
-  height: 90%;
-  aspect-ratio: 16 / 9;
-  padding: 0.5rem;
-  overflow: hidden;
+const BgImage = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  /* background: url();
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover; */
+  /* filter: blur(4px); */
+  background: linear-gradient(#5e5e5e, #000000);
+  z-index: -1;
 `;
 
 const AppLayout = () => {
   return (
-    <AppWrapper>
-      <Applet>
-        <Videos />
-      </Applet>
-    </AppWrapper>
+    <App>
+      <BgImage />
+      <Videos />
+    </App>
   );
 };
 
