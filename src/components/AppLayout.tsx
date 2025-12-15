5be57b0696786fe074a5d859b184d232b71ea3f9
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-12-15 17:57:23 +0000

    feat: add state to track current component to load into view

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,30 +1,36 @@
 import styled from "styled-components";
 import Videos from "./Videos";
+import { useState } from "react";
 
 const App = styled.main`
   height: 100vh;
   height: 100dvh;
   position: relative;
-`;
-
-const BgImage = styled.div`
-  position: absolute;
-  height: 100%;
-  width: 100%;
+  background: red;
   /* background: url();
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover; */
+`;
+
+const BgScreen = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
   /* filter: blur(4px); */
-  background: linear-gradient(#5e5e5e, #000000);
-  z-index: -1;
+  background: rgba(0, 0, 0, 0.8);
+  /* z-index: -1; */
+  user-select: none;
 `;
 
 const AppLayout = () => {
+  const [activeScene, setActiveScene] = useState<string | null>(null);
+
   return (
     <App>
-      <BgImage />
-      <Videos />
+      {activeScene && <BgScreen />}
+      {activeScene === "videos" && <Videos />}
+      <button onClick={() => setActiveScene("videos")}>Videos</button>
     </App>
   );
 };
