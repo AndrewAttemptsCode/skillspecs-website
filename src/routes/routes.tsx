df7945b1b8ca62b894d2850ba68f307367666cbd
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-17 17:43:48 +0000

    feat: adjust root element to HomePage

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import AppLayout from "../components/AppLayout";
+import HomePage from "../pages/HomePage";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout />,
+    element: <HomePage />,
   },
 ]);
 
