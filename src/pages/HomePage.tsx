5b868b396c3214c3df865d7827c7bf69a32c6d6c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-17 20:46:56 +0000

    style: align the items in the center of the page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import NavBar from "../components/NavBar";
 
 const HomePage = () => {
   return (
-    <div className="flex min-h-dvh justify-end bg-emerald-950">
+    <div className="flex min-h-dvh items-center justify-end bg-emerald-900 p-2">
       <NavBar />
     </div>
   );
