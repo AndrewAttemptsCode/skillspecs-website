ba5d659a29ac022b997075d6058aecceda44e905
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-17 20:44:49 +0000

    feat: add data for the navigation items of the NavBar

---
+++ src/data/NavList.ts
@@ -0,0 +1,39 @@
+import { FaInfo, FaTwitch, FaYoutube } from "react-icons/fa6";
+import { IoMdShare } from "react-icons/io";
+import type { IconType } from "react-icons";
+
+export type NavListData = {
+  id: number;
+  label: string;
+  Icon: IconType;
+  styles: string;
+};
+
+const navList: NavListData[] = [
+  {
+    id: 1,
+    label: "videos",
+    styles: "group-hover:fill-[#FF0000]",
+    Icon: FaYoutube,
+  },
+  {
+    id: 2,
+    label: "livestream",
+    styles: "group-hover:fill-[#9146FF]",
+    Icon: FaTwitch,
+  },
+  {
+    id: 3,
+    label: "socials",
+    styles: "group-hover:fill-blue-400",
+    Icon: IoMdShare,
+  },
+  {
+    id: 4,
+    label: "about",
+    styles: "group-hover:fill-gray-400",
+    Icon: FaInfo,
+  },
+];
+
+export default navList;
