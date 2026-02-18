b60493ca309c4a19c925ca3cb3c7acd858f6b436
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-18 18:01:43 +0000

    feat: add SiteLayout component as structure and pass VideosPage as a child via Outlet

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import SiteLayout from "../components/SiteLayout";
+import VideosPage from "../pages/VideosPage";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    element: <SiteLayout />,
+    children: [
+      {
+        path: "videos",
+        element: <VideosPage />,
+      },
+    ],
   },
 ]);
 
