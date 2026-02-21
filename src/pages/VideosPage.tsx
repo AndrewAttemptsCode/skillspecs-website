514bf76a21d619df0fb7b4c48d9c67d7ed67bc44
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-21 20:00:39 +0000

    feat: add page contents with video player and video list

--- src/pages/VideosPage.tsx
+++ src/pages/VideosPage.tsx
@@ -1,6 +1,18 @@
+import VideoList from "../components/VideoList";
+import VideoPlayer from "../components/VideoPlayer";
+import useVideos from "../hooks/useVideos";
+
 const VideosPage = () => {
+  const { videos, selectedVideo, loading, error } = useVideos();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-    <div>videos page</div>
+    <div className="min-h-dvh bg-emerald-900">
+      {selectedVideo && <VideoPlayer video={selectedVideo} />}
+      {videos && <VideoList />}
+    </div>
   );
 };
 
