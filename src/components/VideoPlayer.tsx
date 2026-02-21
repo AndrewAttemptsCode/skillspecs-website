063aee385f00e6aefbffacde5542b2021885ab8d
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-21 19:50:25 +0000

    feat: add embed video player layout, border image and iframe

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,9 +1,46 @@
-const VideoPlayer = () => {
+import type { Video } from "../hooks/useVideos";
+import border from "../assets/images/borders/border.webp";
+
+type VideoProps = {
+  video: Video;
+}
+
+const VideoPlayer = ({ video }: VideoProps) => {
   return (
-    <div>
-      
+    <div className="flex justify-center py-4">
+      <section
+        aria-label="Youtube video player"
+        className="w-[90%] max-w-5xl aspect-video relative p-1"
+      >
+        <div
+          style={{ backgroundImage: `url(${border})` }}
+          className="absolute inset-0 bg-contain bg-center bg-no-repeat pointer-events-none"
+        />
+        <iframe
+          src={`https://www.youtube.com/embed/${video.videoId}`}
+          title={video.title}
+          allow="encrypted-media; picture-in-picture;"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="size-full border-0"
+        ></iframe>
+      </section>
     </div>
   );
 };
 
 export default VideoPlayer;
+
+// const PlayerContainer = styled.section`
+//   width: min(90%, 800px);
+//   aspect-ratio: 16 / 9;
+//   margin: 0 auto;
+//   animation: ${LoadIn} 1s ease forwards;
+
+//   iframe {
+//     border: none;
+//     height: 100%;
+//     width: 100%;
+//     border-radius: 8px;
+//   }
+// `;
