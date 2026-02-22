b4925b8ab8aea669131b700c2f42adf46e889689
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-22 20:58:53 +0000

    feat: add Logo to header section and adjust header layout control

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
+import Logo from "./Logo";
 
 const SiteLayout = () => {
   return (
     <div className="bg-emerald-900">
-      <header className="flex justify-between items-center p-4">
+      <header className="flex flex-col sm:flex-row sm:justify-between items-center gap-2 sm:gap-0 p-2 sm:p-4">
+        <Logo />
         <NavBar selectedItems={["/", "/videos", "/livestream", "/socials", "/about"]} orientation="horizontal" />
       </header>
       <main>
