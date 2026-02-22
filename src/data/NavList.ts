aa95f4670e8ae6e5971ca61f1a34dd0a104b024b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-22 18:16:59 +0000

    feat: add path property to navList items, home object and updated focus to focus-visible

--- src/data/NavList.ts
+++ src/data/NavList.ts
@@ -1,9 +1,11 @@
-import { FaInfo, FaTwitch, FaYoutube } from "react-icons/fa6";
+import { FaTwitch, FaYoutube } from "react-icons/fa6";
 import { IoMdShare } from "react-icons/io";
 import type { IconType } from "react-icons";
+import { FaHome, FaInfoCircle } from "react-icons/fa";
 
 export type NavListData = {
   id: number;
+  path: string;
   label: string;
   Icon: IconType;
   styles: string;
@@ -11,28 +13,39 @@ export type NavListData = {
 
 const navList: NavListData[] = [
   {
+    id: 0,
+    path: "/",
+    label: "Home",
+    styles: "group-hover:fill-[#7FBF3F] group-focus-visible:fill-[#7FBF3F]",
+    Icon: FaHome,
+  },
+  {
     id: 1,
-    label: "videos",
-    styles: "group-hover:fill-[#FF0000] group-focus:fill-[#FF0000]",
+    path: "videos",
+    label: "Videos",
+    styles: "group-hover:fill-[#FF0000] group-focus-visible:fill-[#FF0000]",
     Icon: FaYoutube,
   },
   {
     id: 2,
-    label: "livestream",
-    styles: "group-hover:fill-[#9146FF] group-focus:fill-[#9146FF]",
+    path: "livestream",
+    label: "Livestream",
+    styles: "group-hover:fill-[#9146FF] group-focus-visible:fill-[#9146FF]",
     Icon: FaTwitch,
   },
   {
     id: 3,
-    label: "socials",
-    styles: "group-hover:fill-blue-400 group-focus:fill-blue-400",
+    path: "socials",
+    label: "Socials",
+    styles: "group-hover:fill-blue-400 group-focus-visible:fill-blue-400",
     Icon: IoMdShare,
   },
   {
     id: 4,
-    label: "about",
-    styles: "group-hover:fill-gray-400 group-focus:fill-gray-400",
-    Icon: FaInfo,
+    path: "about",
+    label: "About Skill Specs",
+    styles: "group-hover:fill-gray-400 group-focus-visible:fill-gray-400",
+    Icon: FaInfoCircle,
   },
 ];
 
