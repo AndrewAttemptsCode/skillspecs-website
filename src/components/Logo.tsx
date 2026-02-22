52cd9ce3a7282a68994ccddcbdb63c1bc51655e7
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-22 20:57:39 +0000

    feat: add initial brand logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div className="p-3 sm:p-4 bg-emerald-950 rounded-2xl">
+      <h1 className="font-heading text-[#7FBF3F] text-2xl">Skill Specs</h1>
+    </div>
+  );
+};
+
+export default Logo;
