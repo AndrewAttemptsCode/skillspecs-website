1b610be0dc3e23507fd394081e7a2d602dcf56b7
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-22 19:04:11 +0000

    feat: pass orientation prop as vertical

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 const HomePage = () => {
   return (
     <div className="flex min-h-dvh items-center justify-end bg-emerald-900 p-2">
-      <NavBar />
+      <NavBar selectedItems={["videos", "livestream", "socials", "about"]} orientation="vertical" />
     </div>
   );
 };
