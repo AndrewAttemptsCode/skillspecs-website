082866a427be5f69445f62937c408076df68c8aa
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-24 22:22:04 +0000

    feat: add extra image and use mask as a src to apply a drop shadow to for the current selected video

--- src/components/VideoListCard.tsx
+++ src/components/VideoListCard.tsx
@@ -3,11 +3,20 @@ import mask from "../assets/images/masks/button_brush1.png";
 
 type CardProps = {
   video: Video;
+  selectedVideo: Video | null;
 }
 
-const VideoListCard = ({ video }: CardProps) => {
+const VideoListCard = ({ video, selectedVideo }: CardProps) => {
+
+  const highlight = video.videoId === selectedVideo?.videoId;
+
   return (
-    <li>
+    <li className="relative">
+      <img
+        src={mask} alt=""
+        className="absolute inset-0 size-full"
+        style={{ filter: highlight ? ("drop-shadow(0 0 2px gold) drop-shadow(0 0 3px gold) drop-shadow(0 0 4px gold)") : "" }}
+      />
       <img 
         src={video.thumbnail} alt=""
         className="aspect-video mask-size-[100%_100%] mask-center mask-no-repeat"
