752c06cb55433102ea13fe66ff2ef3d09a0ee2b8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-24 01:57:42 +0000

    feat: add useBackground hook to display the current page pathname background image

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Logo from "./Logo";
+import useBackground from "../hooks/useBackground";
 
 const SiteLayout = () => {
+  const bg = useBackground();
+
   return (
-    <div className="bg-emerald-900">
+    <div
+      className="bg-emerald-900 bg-no-repeat bg-center bg-cover bg-fixed flex flex-col min-h-dvh"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
       <header className="flex flex-col sm:flex-row sm:justify-between items-center gap-2 sm:gap-0 p-2 sm:p-4">
         <Logo />
         <NavBar selectedItems={["/", "/videos", "/livestream", "/socials", "/about"]} orientation="horizontal" />
       </header>
-      <main>
+      <main className="flex-1">
         <Outlet />
       </main>
       <footer></footer>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,26 @@
 import NavBar from "../components/NavBar";
+import useBackground from "../hooks/useBackground";
 
 const HomePage = () => {
+  const bg = useBackground();
+
   return (
-    <div className="flex min-h-dvh items-center justify-end bg-emerald-900 p-2">
-      <NavBar selectedItems={["/videos", "/livestream", "/socials", "/about"]} orientation="vertical" />
+    <div
+      className="flex min-h-dvh justify-center lg:justify-end p-4 bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `
+          url(${bg}),
+          radial-gradient(
+            circle at center,
+          rgba(180,255,220,0.8) 0%,
+          rgba(100,255,180,0.4) 25%,
+          rgba(0,255,150,0.15) 40%,
+            rgba(0,0,0,0) 55%),
+          linear-gradient(to top, #064e3b, #558578)
+        `,
+      }}
+    >
+      <NavBar selectedItems={["/videos", "/livestream", "/socials", "/about"]} orientation="horizontal" />
     </div>
   );
 };
