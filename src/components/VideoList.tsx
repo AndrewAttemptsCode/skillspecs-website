ec27cd467d4e03e3c9a4d6a01d51b6c59dda7939
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-25 16:47:51 +0000

    style: add backdrop blur to the background container to give it seperation from the background image

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -9,8 +9,8 @@ type ListProps = {
 const VideoList = ({ videos, selectedVideo }: ListProps) => {
   return (
     <div>
-      <section className="w-[90%] max-w-330 p-4 mx-auto bg-white/25">
-        <h2 className="font-heading text-[#7FBF3F] p-2 bg-black/70 text-xl md:text-2xl mb-4">Latest Videos...</h2>
+      <section className="w-[90%] max-w-330 p-4 mx-auto bg-white/10 backdrop-blur-sm">
+        <h2 className="font-heading text-[#7FBF3F] p-2 bg-black/40 backdrop-blur-sm text-xl md:text-2xl mb-4">Latest Videos...</h2>
         <ul className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-4">
           {videos.map(video => (
             <VideoListCard key={video.videoId} video={video} selectedVideo={selectedVideo} />
