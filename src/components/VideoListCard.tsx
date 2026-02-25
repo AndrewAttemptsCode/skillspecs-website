45b2213b8727cbb7863a4ed09c18db2938ffa601
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-25 17:12:20 +0000

    feat: add button wrapped around the card content with onClick event handler to update the selected video with the current video object content

--- src/components/VideoListCard.tsx
+++ src/components/VideoListCard.tsx
@@ -4,24 +4,27 @@ import mask from "../assets/images/masks/button_brush1.png";
 type CardProps = {
   video: Video;
   selectedVideo: Video | null;
+  updateSelected: (videoDetails: Video) => void;
 }
 
-const VideoListCard = ({ video, selectedVideo }: CardProps) => {
+const VideoListCard = ({ video, selectedVideo, updateSelected }: CardProps) => {
 
   const highlight = video.videoId === selectedVideo?.videoId;
 
   return (
     <li className="relative">
-      <img
-        src={mask} alt=""
-        className="absolute inset-0 size-full"
-        style={{ filter: highlight ? ("drop-shadow(0 0 1px gold) drop-shadow(0 0 1px gold) drop-shadow(0 0 1px gold)") : "" }}
-      />
-      <img 
-        src={video.thumbnail} alt=""
-        className="aspect-video mask-size-[100%_100%] mask-center mask-no-repeat"
-        style={{ maskImage: `url(${mask})` }}
-      />
+      <button onClick={() => updateSelected(video)}>
+        <img
+          src={mask} alt=""
+          className="absolute inset-0 size-full"
+          style={{ filter: highlight ? ("drop-shadow(0 0 1px gold) drop-shadow(0 0 1px gold) drop-shadow(0 0 1px gold)") : "" }}
+        />
+        <img 
+          src={video.thumbnail} alt=""
+          className="aspect-video mask-size-[100%_100%] mask-center mask-no-repeat"
+          style={{ maskImage: `url(${mask})` }}
+        />
+      </button>
     </li>
   );
 };
