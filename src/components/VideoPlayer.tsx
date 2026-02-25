17c56d100ff1cd22b48db763b0b19f21ffa0dfac
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-25 20:12:38 +0000

    feat: add ternary render based off if video data is present

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,9 +1,10 @@
 import type { Video } from "../hooks/useVideos";
 import border from "../assets/images/borders/border.webp";
 import { useEffect } from "react";
+import VideoPlayerSkeleton from "./VideoPlayerSkeleton";
 
 type VideoProps = {
-  video: Video;
+  video: Video | null;
 };
 
 const VideoPlayer = ({ video }: VideoProps) => {
@@ -16,24 +17,30 @@ const VideoPlayer = ({ video }: VideoProps) => {
   }, [video]);
 
   return (
-    <div className="flex justify-center py-4">
-      <section
-        aria-label="Youtube video player"
-        className="relative aspect-video w-[90%] max-w-5xl p-1"
-      >
-        <div
-          style={{ backgroundImage: `url(${border})` }}
-          className="pointer-events-none absolute inset-0 bg-contain bg-center bg-no-repeat"
-        />
-        <iframe
-          src={`https://www.youtube.com/embed/${video.videoId}`}
-          title={video.title}
-          allow="encrypted-media; picture-in-picture;"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-          className="size-full border-0"
-        ></iframe>
-      </section>
+    <div>
+      <div className="w-[90%] max-w-5xl mx-auto py-4">
+        {!video ? (
+          <VideoPlayerSkeleton />
+        ) : (
+          <section
+            aria-label="Youtube video player"
+            className="relative aspect-video p-1"
+          >
+            <div
+              style={{ backgroundImage: `url(${border})` }}
+              className="pointer-events-none absolute inset-0 bg-contain bg-center bg-no-repeat"
+            />
+            <iframe
+              src={`https://www.youtube.com/embed/${video.videoId}`}
+              title={video.title}
+              allow="encrypted-media; picture-in-picture;"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              className="size-full border-0"
+            ></iframe>
+          </section>
+        )}
+      </div>
     </div>
   );
 };
