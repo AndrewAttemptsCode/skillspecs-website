15aba7bdf315629c119f103975b9e1acd4bb9adf
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-26 20:53:52 +0000

    feat: add error display logic

---
+++ src/components/ErrorDisplay.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+type ErrorProps = {
+  error: string;
+}
+
+const ErrorDisplay = ({ error }: ErrorProps) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [retries, setRetries] = useState(0);
+  const [waiting, setWaiting] = useState(false);
+  const [countDown, setCountDown] = useState(3);
+
+  const intervalRef = useRef<number | null>(null);
+  const timeoutRef = useRef<number | null>(null);
+
+  const showFinalMessage = retries === 3 && !waiting;
+  const message = showFinalMessage ? "Man! ...Please let me know it is broken." : error;
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    }
+  }, []);
+
+  const handleRetry = () => {
+    if (retries >= 3) return;
+
+    setWaiting(true);
+    setRetries((prev) => prev + 1);
+    setCountDown(3);
+
+    intervalRef.current = setInterval(() => {
+      setCountDown((prev) => (prev > 0 ? prev - 1 : 0)); 
+    }, 1000);
+
+    timeoutRef.current = setTimeout(() => {
+      navigate(location.pathname);
+      setWaiting(false);
+
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    }, 3000);
+  }
+
+  return (
+    <div>
+      <section>
+        <h2>{message}</h2>
+        {waiting && <p>Retrying in {countDown}...</p>}
+        <button onClick={handleRetry} disabled={waiting || retries === 3}>Try again</button>
+      </section>
+    </div>
+  );
+};
+
+export default ErrorDisplay;
