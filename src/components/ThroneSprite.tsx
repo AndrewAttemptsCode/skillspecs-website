20c4b1292d1c35df23a4e00e935d5d88942baf9b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-03 03:03:36 +0000

    style: add animate-throne animation to the bg

--- src/components/ThroneSprite.tsx
+++ src/components/ThroneSprite.tsx
@@ -3,18 +3,15 @@ import sprite from "../assets/images/sprites/throne_sprite.png";
 const ThroneSprite = () => {
   return (
     <div
-      className="absolute bottom-1/4 left-1/2 -translate-x-1/2 bg-no-repeat"
+      className="animate-throne absolute bottom-1/4 left-1/2 -translate-x-1/2 bg-no-repeat"
       style={{
         backgroundImage: `url(${sprite})`,
         height: "45%",
         width: "auto",
         aspectRatio: "426 / 790",
         backgroundSize: "900% 100%",
-        backgroundPosition: "0 0",
       }}
-    >
-
-    </div>
+    />
   );
 };
 
