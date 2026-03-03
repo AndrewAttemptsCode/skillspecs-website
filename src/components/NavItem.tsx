b1ff2af522b33df4ddc2d955dfa76f4a3dc6ba64
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-03 07:16:24 +0000

    fix: add side effect to inner width if user adjusts window size to allow keyboard tabbing to work

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,7 @@
 import { NavLink, useLocation } from "react-router-dom";
 import stone from "../assets/images/buttons/stone_btn.png";
 import type { NavListData } from "../data/NavList";
+import { useEffect, useState } from "react";
 
 type ItemProps = {
   item: NavListData;
@@ -10,16 +11,25 @@ type ItemProps = {
 const NavItem = ({ item, navOpen }: ItemProps) => {
   const { path, label, Icon, styles, active } = item;
   const location = useLocation();
+  const [isDesktop, setIsDesktop] = useState<boolean>(window.innerWidth >= 1024);
 
   const transitionClasses = "ease transition duration-300";
   const selected = path === location.pathname;
+
+  useEffect(() => {
+    const handleResize = () => setIsDesktop(window.innerWidth >= 1024);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const tabOptions = navOpen || isDesktop ? 0 : -1; 
 
   return (
     <NavLink
       to={path}
       title={label}
-      className={`group transition-transform duration-200 ease-linear block rounded-xl outline-none focus-visible:ring-2 focus-visible:ring-[#7FBF3F] focus-visible:ring-offset-2 focus-visible:ring-offset-transparent ${navOpen ? "translate-x-0" : "translate-x-[150%]"} lg:translate-x-0`}
-      tabIndex={navOpen ? 0 : -1}
+      className={`group lg:tabIndex-0 transition-transform duration-200 ease-linear block rounded-xl outline-none focus-visible:ring-2 focus-visible:ring-[#7FBF3F] focus-visible:ring-offset-2 focus-visible:ring-offset-transparent ${navOpen ? "translate-x-0" : "translate-x-[150%]"} lg:translate-x-0`}
+      tabIndex={tabOptions}
     >
       <img
         src={stone}
