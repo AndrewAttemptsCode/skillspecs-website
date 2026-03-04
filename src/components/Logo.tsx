434c9cd940525a1dae387ad82842cfbf5ad1a3af
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-04 17:05:24 +0000

    feat: update logo with image, styling and link to root

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,24 @@
+import { NavLink } from "react-router-dom";
+import idle from "../assets/images/sprites/idle.png";
+
 const Logo = () => {
   return (
-    <div className="rounded-2xl bg-emerald-950 p-3 sm:p-4">
-      <h1 className="font-heading text-2xl text-[#7FBF3F]">Skill Specs</h1>
-    </div>
+    <NavLink
+      to={"/"}
+      title="Home"
+      className="relative flex flex-col aspect-[21 / 9] h-16 justify-end rounded-md py-1 px-4 outline-none focus-visible:ring-2 focus-visible:ring-[#7FBF3F] focus-visible:ring-offset-2 focus-visible:ring-offset-transparent"
+      style={{ 
+        backgroundImage: "linear-gradient(to bottom, rgba(154, 231, 76, 0.8), rgba(30, 46, 13, 0.8))" 
+      }}
+    >
+      <h1 className="relative font-heading text-shadow-md text-green-200/95 text-xl z-5">
+        Skill Specs
+      </h1>
+      <img
+        src={idle} alt=""
+        className="absolute h-[95%] bottom-0 left-1/2 -translate-x-1/2 drop-shadow-[0_0_4px] drop-shadow-green-100"
+      />
+    </NavLink>
   );
 };
 
