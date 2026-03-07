5d71f8284be483ccd39339c7b0bf083648a39d48
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-07 15:28:45 +0000

    feat: add initial footer content with policy credits

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="p-4 flex items-center bg-linear-to-t from-black via-black/80 via-60% to-transparent">
+      <p className="text-yellow-300 text-pretty max-w-lg">
+        Created using intellectual property belonging to Jagex Limited under the terms of Jagex's Fan Content Policy. This content is not endorsed by or affiliated with Jagex.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
