426d0a055f749329962a0af183fd11de8ce4579b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-07 18:17:43 +0000

    styles: adjust layout

--- src/components/SocialsList.tsx
+++ src/components/SocialsList.tsx
@@ -3,10 +3,14 @@ import SocialCard from "./SocialCard";
 
 const SocialsList = () => {
   return (
-    <section>
-      {socials.map((item) => (
-        <SocialCard key={item.id} item={item} />
-      ))}
+    <section className="py-4">
+      <div className="w-[90%] max-w-5xl mx-auto">
+        <ul className="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4">
+          {socials.map((item) => (
+            <SocialCard key={item.id} item={item} />
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
