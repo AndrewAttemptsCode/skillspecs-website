fe401a96e77d25e406b403d245f4502cc4e60fd8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-08 18:17:20 +0000

    style: add style variants as classes from socials.ts

--- src/components/SocialCard.tsx
+++ src/components/SocialCard.tsx
@@ -10,25 +10,25 @@ const SocialCard = ({ item }: CardProps) => {
   const transition = "transition-all 0.3s ease-in";
 
   return (
-    <li className="group relative bg-amber-50 p-4 mt-8 rounded-md cursor-pointer">
+    <li className={`group relative ${styles.cardBg} p-4 mt-8 rounded-md cursor-pointer`}>
     
-      <div className="absolute inset-0 flex justify-center flex-wrap gap-0.5 overflow-hidden">
+      <div className="absolute inset-0 flex justify-center flex-wrap gap-0.5 overflow-hidden pointer-events-none">
         {[...Array(20)].map((_, i) => (
-          <Icon key={i} className="size-16 fill-black/5" />
+          <Icon key={i} className={`size-16 ${styles.cardBgIcon}`} />
         ))}
       </div>
 
       <a href={path} rel="noopener noreferrer" target="_blank">
-        <div className={`absolute top-0 -translate-y-1/2 left-1/2 -translate-x-1/2 size-16 bg-amber-400 p-3 rounded-md group-hover:-translate-y-[60%] ${transition}`}>
+        <div className={`absolute top-0 -translate-y-1/2 left-1/2 -translate-x-1/2 size-16 p-3 rounded-md group-hover:-translate-y-[60%] ${transition} ${styles.iconBg}`}>
           <Icon 
-            className="size-full"
+            className={`size-full ${styles.icon}`}
           />
         </div>
-        <div>
-          <h2 className={`group-hover:underline ${transition} font-semibold`}>
+        <div className="mt-4">
+          <h2 className={`group-hover:underline ${transition} font-bold text-xl lg:text-2xl ${styles.text}`}>
             {label}
           </h2>
-          <p>{description}</p>
+          <p className="text-white font-semibold">{description}</p>
         </div>
       </a>
     </li>
