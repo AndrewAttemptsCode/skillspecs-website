170742f107cf0a2ea8bbbd50886201b6afd12e04
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-09 18:38:34 +0000

    feat: add business and sponsorship email contact display

---
+++ src/components/Contact.tsx
@@ -0,0 +1,33 @@
+const contactList = [
+  {
+    id: 0,
+    title: "Sponsorship & Partnerships",
+    email: "SkillSpecs@CrusaderTalent.com",
+  },
+  {
+    id: 1,
+    title: "Business General",
+    email: "SkillSpecsOfficial@gmail.com",
+  },
+];
+
+const Contact = () => {
+  return (
+    <div>
+      <h2 className="font-semibold uppercase">Enquiries</h2>
+      <div className="flex flex-col gap-2">
+        {contactList.map((contact) => (
+          <a
+            key={contact.id}
+            href={`mailto:${contact.email}`}
+            className="w-fit rounded-md bg-amber-700 p-2 font-semibold text-amber-50 outline-0 transition-all duration-300 ease-in-out hover:brightness-110 focus-visible:ring-2 focus-visible:ring-amber-900"
+          >
+            {contact.title}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
